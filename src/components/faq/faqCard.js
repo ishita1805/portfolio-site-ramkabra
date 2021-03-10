955fd1e89f1ba6bc0cc344b36104e43953b58aef
Ishita Kabra <ishitakabra1805@gmail.com>
2021-03-10 20:21:36 +0530

    carousel made

--- src/components/faq/faqCard.js
+++ src/components/faq/faqCard.js
@@ -6,7 +6,11 @@ const FaqCard = ({no,quest,ans,...props}) => {
     return (
         <>
         <div className='faq-card'>
-             <h1>{no}</h1>
+             <h1>
+                {no.includes("1")?<span className='fixer-text'>.</span>:null}
+                {no.includes("11")?<span className='fixer-text'>,</span>:null}
+                {no}
+             </h1>
              <p>{quest}</p>
              <i className={clicked?'fa fa-chevron-up':'fa fa-chevron-down'} onClick={()=>setClicked(!clicked)}></i>
         </div>
